f56b9cc9ee1d612f076ea9948ac8dfeab72ba27f
izumin5210 <m@izum.in>
2020-12-09 08:45:10 +0900

    test: setup sub-pacakges for managing protobufs for E2E testing

---
+++ packages/@testapis/node/lib/hello/hello_pb.d.ts
@@ -0,0 +1,70 @@
+// package: testapi.hello
+// file: hello/hello.proto
+
+import * as jspb from "google-protobuf";
+import * as google_protobuf_timestamp_pb from "google-protobuf/google/protobuf/timestamp_pb";
+
+export class User extends jspb.Message {
+  getId(): number;
+  setId(value: number): void;
+
+  getName(): string;
+  setName(value: string): void;
+
+  clearPostsList(): void;
+  getPostsList(): Array<Post>;
+  setPostsList(value: Array<Post>): void;
+  addPosts(value?: Post, index?: number): Post;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): User.AsObject;
+  static toObject(includeInstance: boolean, msg: User): User.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: User, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): User;
+  static deserializeBinaryFromReader(message: User, reader: jspb.BinaryReader): User;
+}
+
+export namespace User {
+  export type AsObject = {
+    id: number,
+    name: string,
+    postsList: Array<Post.AsObject>,
+  };
+}
+
+export class Post extends jspb.Message {
+  getId(): number;
+  setId(value: number): void;
+
+  getTitle(): string;
+  setTitle(value: string): void;
+
+  getBody(): string;
+  setBody(value: string): void;
+
+  hasPublishedTime(): boolean;
+  clearPublishedTime(): void;
+  getPublishedTime(): google_protobuf_timestamp_pb.Timestamp | undefined;
+  setPublishedTime(value?: google_protobuf_timestamp_pb.Timestamp): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): Post.AsObject;
+  static toObject(includeInstance: boolean, msg: Post): Post.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: Post, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): Post;
+  static deserializeBinaryFromReader(message: Post, reader: jspb.BinaryReader): Post;
+}
+
+export namespace Post {
+  export type AsObject = {
+    id: number,
+    title: string,
+    body: string,
+    publishedTime?: google_protobuf_timestamp_pb.Timestamp.AsObject,
+  };
+}
+
