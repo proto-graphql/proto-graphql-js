ea663aed490fdd3b32e5923b8d6f37fa7db5ab5c
izumin5210 <m@izum.in>
2020-12-28 04:42:45 +0000

    test: add integration test with protobuf native

--- packages/protoc-gen-nexus/src/__tests__/__integration__/simpleProto.test.ts
+++ packages/protoc-gen-nexus/src/__tests__/__integration__/simpleProto.test.ts
@@ -2,7 +2,7 @@ import { graphql } from "graphql";
 import { queryField } from "nexus";
 import { testSchemaGeneration } from "../__helpers__/process.test.helper";
 import * as pbjs from "@testapis/node/lib/testapis/hello";
-// import * as pbnative from "@testapis/node-native/lib/testapis/hello/hello_pb";
+import * as pbnative from "@testapis/node-native/lib/testapis/hello/hello_pb";
 
 testSchemaGeneration("hello", "protobufjs", {
   schemaTests: [
@@ -79,76 +79,76 @@ testSchemaGeneration("hello", "protobufjs", {
   ],
 });
 
-// testSchemaGeneration("hello", "native prottobuf", {
-//   schemaTests: [
-//     [
-//       "tests query",
-//       {
-//         test: queryField("test", {
-//           type: "Hello",
-//           resolve() {
-//             const rp = new pbnative.Primitives();
-//             rp.setRequiredDoubleValue(2.4);
-//             rp.setRequiredFloatValue(3.5);
-//             rp.setRequiredInt32Value(2);
-//             rp.setRequiredInt64Value(4);
-//             rp.setRequiredUint32Value(5);
-//             rp.setRequiredUint64Value(6);
-//             rp.setRequiredSint32Value(7);
-//             rp.setRequiredSint64Value(8);
-//             rp.setRequiredFixed32Value(9);
-//             rp.setRequiredFixed64Value(10);
-//             rp.setRequiredSfixed32Value(11);
-//             rp.setRequiredSfixed64Value(12);
-//             rp.setRequiredBoolValue(true);
-//             rp.setRequiredStringValue("foobar");
-//             const hello = new pbnative.Hello();
-//             hello.setRequiredPrimitives(rp);
-//             return hello;
-//           },
-//         }),
-//       },
-//       async (schema) => {
-//         expect(
-//           await graphql(
-//             schema,
-//             `
-//               query Test {
-//                 test {
-//                   requiredPrimitives {
-//                     ...Primitives
-//                   }
-//                   optionalPrimitives {
-//                     ...Primitives
-//                   }
-//                   requiredPrimitivesList {
-//                     ...Primitives
-//                   }
-//                   optionalPrimitivesList {
-//                     ...Primitives
-//                   }
-//                 }
-//               }
-//               fragment Primitives on Primitives {
-//                 requiredDoubleValue
-//                 requiredFloatValue
-//                 requiredInt32Value
-//                 requiredInt64Value
-//                 requiredUint32Value
-//                 requiredUint64Value
-//                 requiredSint32Value
-//                 requiredSint64Value
-//                 requiredFixed32Value
-//                 requiredFixed64Value
-//                 requiredSfixed32Value
-//                 requiredSfixed64Value
-//                 requiredBoolValue
-//                 requiredStringValue
-//               }
-//             `,
-//           ),
-//         ).toMatchSnapshot();
-//       },
-//     ],
-//   ],
-// });
+testSchemaGeneration("hello", "native protobuf", {
+  schemaTests: [
+    [
+      "tests query",
+      {
+        test: queryField("test", {
+          type: "Hello",
+          resolve() {
+            const rp = new pbnative.Primitives();
+            rp.setRequiredDoubleValue(2.4);
+            rp.setRequiredFloatValue(3.5);
+            rp.setRequiredInt32Value(2);
+            rp.setRequiredInt64Value(4);
+            rp.setRequiredUint32Value(5);
+            rp.setRequiredUint64Value(6);
+            rp.setRequiredSint32Value(7);
+            rp.setRequiredSint64Value(8);
+            rp.setRequiredFixed32Value(9);
+            rp.setRequiredFixed64Value(10);
+            rp.setRequiredSfixed32Value(11);
+            rp.setRequiredSfixed64Value(12);
+            rp.setRequiredBoolValue(true);
+            rp.setRequiredStringValue("foobar");
+            const hello = new pbnative.Hello();
+            hello.setRequiredPrimitives(rp);
+            return hello;
+          },
+        }),
+      },
+      async (schema) => {
+        expect(
+          await graphql(
+            schema,
+            `
+              query Test {
+                test {
+                  requiredPrimitives {
+                    ...Primitives
+                  }
+                  optionalPrimitives {
+                    ...Primitives
+                  }
+                  requiredPrimitivesList {
+                    ...Primitives
+                  }
+                  optionalPrimitivesList {
+                    ...Primitives
+                  }
+                }
+              }
+              fragment Primitives on Primitives {
+                requiredDoubleValue
+                requiredFloatValue
+                requiredInt32Value
+                requiredInt64Value
+                requiredUint32Value
+                requiredUint64Value
+                requiredSint32Value
+                requiredSint64Value
+                requiredFixed32Value
+                requiredFixed64Value
+                requiredSfixed32Value
+                requiredSfixed64Value
+                requiredBoolValue
+                requiredStringValue
+              }
+            `
+          )
+        ).toMatchSnapshot();
+      },
+    ],
+  ],
+});
