697e6ac3089303071196ab8ace3c4d856670955a
izumin5210 <m@izum.in>
2022-11-19 15:23:06 +0900

    update

--- packages/e2e/nexus/jest.config.js
+++ packages/e2e/nexus/jest.config.js
@@ -4,16 +4,18 @@ const path = require("path");
 module.exports = {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   projects: require("glob")
-    .sync("./**/tsconfig.json")
-    .map((tsconfigPath) => ({
-      preset: "ts-jest",
-      testEnvironment: "node",
-      rootDir: path.dirname(tsconfigPath),
-      globals: {
-        "ts-jest": {
-          tsconfig: tsconfigPath,
-          diagnostics: true,
-        },
-      },
-    })),
+    .sync("./e2e/*/tsconfig.json")
+    .map((tsconfigPath) => {
+      return {
+        preset: "ts-jest",
+        testEnvironment: "node",
+        rootDir: path.dirname(tsconfigPath),
+      };
+    }),
+  globals: {
+    "ts-jest": {
+      // typecheck on other process
+      diagnostics: false,
+    },
+  },
 };
