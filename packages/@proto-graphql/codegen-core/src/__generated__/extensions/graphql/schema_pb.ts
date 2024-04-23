767076717a4eb7fdf84991f851f475e2c92a074e
izumin5210 <m@izum.in>
2024-04-23 22:41:45 +0900

    `./scripts/compile-extensions-proto`

--- packages/@proto-graphql/codegen-core/src/__generated__/extensions/graphql/schema_pb.ts
+++ packages/@proto-graphql/codegen-core/src/__generated__/extensions/graphql/schema_pb.ts
@@ -3,24 +3,8 @@
 /* eslint-disable */
 // @ts-nocheck
 
-import type {
-  BinaryReadOptions,
-  FieldList,
-  JsonReadOptions,
-  JsonValue,
-  PartialMessage,
-  PlainMessage,
-} from "@bufbuild/protobuf";
-import {
-  EnumOptions,
-  EnumValueOptions,
-  FieldOptions,
-  FileOptions,
-  Message,
-  MessageOptions,
-  OneofOptions,
-  proto3,
-} from "@bufbuild/protobuf";
+import type { BinaryReadOptions, FieldList, JsonReadOptions, JsonValue, PartialMessage, PlainMessage } from "@bufbuild/protobuf";
+import { EnumOptions, EnumValueOptions, FieldOptions, FileOptions, Message, MessageOptions, OneofOptions, proto3 } from "@bufbuild/protobuf";
 
 /**
  * @generated from enum graphql.Nullability
@@ -80,52 +64,25 @@ export class GraphqlSchemaOptions extends Message<GraphqlSchemaOptions> {
   static readonly runtime: typeof proto3 = proto3;
   static readonly typeName = "graphql.GraphqlSchemaOptions";
   static readonly fields: FieldList = proto3.util.newFieldList(() => [
-    {
-      no: 1,
-      name: "type_prefix",
-      kind: "scalar",
-      T: 9 /* ScalarType.STRING */,
-    },
-    {
-      no: 2,
-      name: "ignore_requests",
-      kind: "scalar",
-      T: 8 /* ScalarType.BOOL */,
-    },
-    {
-      no: 3,
-      name: "ignore_responses",
-      kind: "scalar",
-      T: 8 /* ScalarType.BOOL */,
-    },
+    { no: 1, name: "type_prefix", kind: "scalar", T: 9 /* ScalarType.STRING */ },
+    { no: 2, name: "ignore_requests", kind: "scalar", T: 8 /* ScalarType.BOOL */ },
+    { no: 3, name: "ignore_responses", kind: "scalar", T: 8 /* ScalarType.BOOL */ },
     { no: 4, name: "ignore", kind: "scalar", T: 8 /* ScalarType.BOOL */ },
   ]);
 
-  static fromBinary(
-    bytes: Uint8Array,
-    options?: Partial<BinaryReadOptions>,
-  ): GraphqlSchemaOptions {
+  static fromBinary(bytes: Uint8Array, options?: Partial<BinaryReadOptions>): GraphqlSchemaOptions {
     return new GraphqlSchemaOptions().fromBinary(bytes, options);
   }
 
-  static fromJson(
-    jsonValue: JsonValue,
-    options?: Partial<JsonReadOptions>,
-  ): GraphqlSchemaOptions {
+  static fromJson(jsonValue: JsonValue, options?: Partial<JsonReadOptions>): GraphqlSchemaOptions {
     return new GraphqlSchemaOptions().fromJson(jsonValue, options);
   }
 
-  static fromJsonString(
-    jsonString: string,
-    options?: Partial<JsonReadOptions>,
-  ): GraphqlSchemaOptions {
+  static fromJsonString(jsonString: string, options?: Partial<JsonReadOptions>): GraphqlSchemaOptions {
     return new GraphqlSchemaOptions().fromJsonString(jsonString, options);
   }
 
-  static equals(
-    a: GraphqlSchemaOptions | PlainMessage<GraphqlSchemaOptions> | undefined,
-    b: GraphqlSchemaOptions | PlainMessage<GraphqlSchemaOptions> | undefined,
-  ): boolean {
+  static equals(a: GraphqlSchemaOptions | PlainMessage<GraphqlSchemaOptions> | undefined, b: GraphqlSchemaOptions | PlainMessage<GraphqlSchemaOptions> | undefined): boolean {
     return proto3.util.equals(GraphqlSchemaOptions, a, b);
   }
 }
@@ -203,37 +160,19 @@ export class GraphqlObjectTypeOptions extends Message<GraphqlObjectTypeOptions> 
     { no: 4, name: "name", kind: "scalar", T: 9 /* ScalarType.STRING */ },
   ]);
 
-  static fromBinary(
-    bytes: Uint8Array,
-    options?: Partial<BinaryReadOptions>,
-  ): GraphqlObjectTypeOptions {
+  static fromBinary(bytes: Uint8Array, options?: Partial<BinaryReadOptions>): GraphqlObjectTypeOptions {
     return new GraphqlObjectTypeOptions().fromBinary(bytes, options);
   }
 
-  static fromJson(
-    jsonValue: JsonValue,
-    options?: Partial<JsonReadOptions>,
-  ): GraphqlObjectTypeOptions {
+  static fromJson(jsonValue: JsonValue, options?: Partial<JsonReadOptions>): GraphqlObjectTypeOptions {
     return new GraphqlObjectTypeOptions().fromJson(jsonValue, options);
   }
 
-  static fromJsonString(
-    jsonString: string,
-    options?: Partial<JsonReadOptions>,
-  ): GraphqlObjectTypeOptions {
+  static fromJsonString(jsonString: string, options?: Partial<JsonReadOptions>): GraphqlObjectTypeOptions {
     return new GraphqlObjectTypeOptions().fromJsonString(jsonString, options);
   }
 
-  static equals(
-    a:
-      | GraphqlObjectTypeOptions
-      | PlainMessage<GraphqlObjectTypeOptions>
-      | undefined,
-    b:
-      | GraphqlObjectTypeOptions
-      | PlainMessage<GraphqlObjectTypeOptions>
-      | undefined,
-  ): boolean {
+  static equals(a: GraphqlObjectTypeOptions | PlainMessage<GraphqlObjectTypeOptions> | undefined, b: GraphqlObjectTypeOptions | PlainMessage<GraphqlObjectTypeOptions> | undefined): boolean {
     return proto3.util.equals(GraphqlObjectTypeOptions, a, b);
   }
 }
@@ -268,37 +207,19 @@ export class GraphqlInputTypeOptions extends Message<GraphqlInputTypeOptions> {
     { no: 2, name: "ignore", kind: "scalar", T: 8 /* ScalarType.BOOL */ },
   ]);
 
-  static fromBinary(
-    bytes: Uint8Array,
-    options?: Partial<BinaryReadOptions>,
-  ): GraphqlInputTypeOptions {
+  static fromBinary(bytes: Uint8Array, options?: Partial<BinaryReadOptions>): GraphqlInputTypeOptions {
     return new GraphqlInputTypeOptions().fromBinary(bytes, options);
   }
 
-  static fromJson(
-    jsonValue: JsonValue,
-    options?: Partial<JsonReadOptions>,
-  ): GraphqlInputTypeOptions {
+  static fromJson(jsonValue: JsonValue, options?: Partial<JsonReadOptions>): GraphqlInputTypeOptions {
     return new GraphqlInputTypeOptions().fromJson(jsonValue, options);
   }
 
-  static fromJsonString(
-    jsonString: string,
-    options?: Partial<JsonReadOptions>,
-  ): GraphqlInputTypeOptions {
+  static fromJsonString(jsonString: string, options?: Partial<JsonReadOptions>): GraphqlInputTypeOptions {
     return new GraphqlInputTypeOptions().fromJsonString(jsonString, options);
   }
 
-  static equals(
-    a:
-      | GraphqlInputTypeOptions
-      | PlainMessage<GraphqlInputTypeOptions>
-      | undefined,
-    b:
-      | GraphqlInputTypeOptions
-      | PlainMessage<GraphqlInputTypeOptions>
-      | undefined,
-  ): boolean {
+  static equals(a: GraphqlInputTypeOptions | PlainMessage<GraphqlInputTypeOptions> | undefined, b: GraphqlInputTypeOptions | PlainMessage<GraphqlInputTypeOptions> | undefined): boolean {
     return proto3.util.equals(GraphqlInputTypeOptions, a, b);
   }
 }
@@ -390,58 +311,26 @@ export class GraphqlFieldOptions extends Message<GraphqlFieldOptions> {
   static readonly fields: FieldList = proto3.util.newFieldList(() => [
     { no: 1, name: "ignore", kind: "scalar", T: 8 /* ScalarType.BOOL */ },
     { no: 2, name: "name", kind: "scalar", T: 9 /* ScalarType.STRING */ },
-    {
-      no: 3,
-      name: "skip_resolver",
-      kind: "scalar",
-      T: 8 /* ScalarType.BOOL */,
-    },
+    { no: 3, name: "skip_resolver", kind: "scalar", T: 8 /* ScalarType.BOOL */ },
     { no: 4, name: "id", kind: "scalar", T: 8 /* ScalarType.BOOL */ },
-    {
-      no: 11,
-      name: "output_nullability",
-      kind: "enum",
-      T: proto3.getEnumType(Nullability),
-    },
-    {
-      no: 12,
-      name: "input_nullability",
-      kind: "enum",
-      T: proto3.getEnumType(Nullability),
-    },
-    {
-      no: 13,
-      name: "partial_input_nullability",
-      kind: "enum",
-      T: proto3.getEnumType(Nullability),
-    },
+    { no: 11, name: "output_nullability", kind: "enum", T: proto3.getEnumType(Nullability) },
+    { no: 12, name: "input_nullability", kind: "enum", T: proto3.getEnumType(Nullability) },
+    { no: 13, name: "partial_input_nullability", kind: "enum", T: proto3.getEnumType(Nullability) },
   ]);
 
-  static fromBinary(
-    bytes: Uint8Array,
-    options?: Partial<BinaryReadOptions>,
-  ): GraphqlFieldOptions {
+  static fromBinary(bytes: Uint8Array, options?: Partial<BinaryReadOptions>): GraphqlFieldOptions {
     return new GraphqlFieldOptions().fromBinary(bytes, options);
   }
 
-  static fromJson(
-    jsonValue: JsonValue,
-    options?: Partial<JsonReadOptions>,
-  ): GraphqlFieldOptions {
+  static fromJson(jsonValue: JsonValue, options?: Partial<JsonReadOptions>): GraphqlFieldOptions {
     return new GraphqlFieldOptions().fromJson(jsonValue, options);
   }
 
-  static fromJsonString(
-    jsonString: string,
-    options?: Partial<JsonReadOptions>,
-  ): GraphqlFieldOptions {
+  static fromJsonString(jsonString: string, options?: Partial<JsonReadOptions>): GraphqlFieldOptions {
     return new GraphqlFieldOptions().fromJsonString(jsonString, options);
   }
 
-  static equals(
-    a: GraphqlFieldOptions | PlainMessage<GraphqlFieldOptions> | undefined,
-    b: GraphqlFieldOptions | PlainMessage<GraphqlFieldOptions> | undefined,
-  ): boolean {
+  static equals(a: GraphqlFieldOptions | PlainMessage<GraphqlFieldOptions> | undefined, b: GraphqlFieldOptions | PlainMessage<GraphqlFieldOptions> | undefined): boolean {
     return proto3.util.equals(GraphqlFieldOptions, a, b);
   }
 }
@@ -466,31 +355,19 @@ export class GraphqlOneofOptions extends Message<GraphqlOneofOptions> {
     { no: 1, name: "ignore", kind: "scalar", T: 8 /* ScalarType.BOOL */ },
   ]);
 
-  static fromBinary(
-    bytes: Uint8Array,
-    options?: Partial<BinaryReadOptions>,
-  ): GraphqlOneofOptions {
+  static fromBinary(bytes: Uint8Array, options?: Partial<BinaryReadOptions>): GraphqlOneofOptions {
     return new GraphqlOneofOptions().fromBinary(bytes, options);
   }
 
-  static fromJson(
-    jsonValue: JsonValue,
-    options?: Partial<JsonReadOptions>,
-  ): GraphqlOneofOptions {
+  static fromJson(jsonValue: JsonValue, options?: Partial<JsonReadOptions>): GraphqlOneofOptions {
     return new GraphqlOneofOptions().fromJson(jsonValue, options);
   }
 
-  static fromJsonString(
-    jsonString: string,
-    options?: Partial<JsonReadOptions>,
-  ): GraphqlOneofOptions {
+  static fromJsonString(jsonString: string, options?: Partial<JsonReadOptions>): GraphqlOneofOptions {
     return new GraphqlOneofOptions().fromJsonString(jsonString, options);
   }
 
-  static equals(
-    a: GraphqlOneofOptions | PlainMessage<GraphqlOneofOptions> | undefined,
-    b: GraphqlOneofOptions | PlainMessage<GraphqlOneofOptions> | undefined,
-  ): boolean {
+  static equals(a: GraphqlOneofOptions | PlainMessage<GraphqlOneofOptions> | undefined, b: GraphqlOneofOptions | PlainMessage<GraphqlOneofOptions> | undefined): boolean {
     return proto3.util.equals(GraphqlOneofOptions, a, b);
   }
 }
@@ -536,31 +413,19 @@ export class GraphqlEnumOptions extends Message<GraphqlEnumOptions> {
     { no: 4, name: "name", kind: "scalar", T: 9 /* ScalarType.STRING */ },
   ]);
 
-  static fromBinary(
-    bytes: Uint8Array,
-    options?: Partial<BinaryReadOptions>,
-  ): GraphqlEnumOptions {
+  static fromBinary(bytes: Uint8Array, options?: Partial<BinaryReadOptions>): GraphqlEnumOptions {
     return new GraphqlEnumOptions().fromBinary(bytes, options);
   }
 
-  static fromJson(
-    jsonValue: JsonValue,
-    options?: Partial<JsonReadOptions>,
-  ): GraphqlEnumOptions {
+  static fromJson(jsonValue: JsonValue, options?: Partial<JsonReadOptions>): GraphqlEnumOptions {
     return new GraphqlEnumOptions().fromJson(jsonValue, options);
   }
 
-  static fromJsonString(
-    jsonString: string,
-    options?: Partial<JsonReadOptions>,
-  ): GraphqlEnumOptions {
+  static fromJsonString(jsonString: string, options?: Partial<JsonReadOptions>): GraphqlEnumOptions {
     return new GraphqlEnumOptions().fromJsonString(jsonString, options);
   }
 
-  static equals(
-    a: GraphqlEnumOptions | PlainMessage<GraphqlEnumOptions> | undefined,
-    b: GraphqlEnumOptions | PlainMessage<GraphqlEnumOptions> | undefined,
-  ): boolean {
+  static equals(a: GraphqlEnumOptions | PlainMessage<GraphqlEnumOptions> | undefined, b: GraphqlEnumOptions | PlainMessage<GraphqlEnumOptions> | undefined): boolean {
     return proto3.util.equals(GraphqlEnumOptions, a, b);
   }
 }
@@ -585,37 +450,19 @@ export class GraphqlEnumValueOptions extends Message<GraphqlEnumValueOptions> {
     { no: 1, name: "ignore", kind: "scalar", T: 8 /* ScalarType.BOOL */ },
   ]);
 
-  static fromBinary(
-    bytes: Uint8Array,
-    options?: Partial<BinaryReadOptions>,
-  ): GraphqlEnumValueOptions {
+  static fromBinary(bytes: Uint8Array, options?: Partial<BinaryReadOptions>): GraphqlEnumValueOptions {
     return new GraphqlEnumValueOptions().fromBinary(bytes, options);
   }
 
-  static fromJson(
-    jsonValue: JsonValue,
-    options?: Partial<JsonReadOptions>,
-  ): GraphqlEnumValueOptions {
+  static fromJson(jsonValue: JsonValue, options?: Partial<JsonReadOptions>): GraphqlEnumValueOptions {
     return new GraphqlEnumValueOptions().fromJson(jsonValue, options);
   }
 
-  static fromJsonString(
-    jsonString: string,
-    options?: Partial<JsonReadOptions>,
-  ): GraphqlEnumValueOptions {
+  static fromJsonString(jsonString: string, options?: Partial<JsonReadOptions>): GraphqlEnumValueOptions {
     return new GraphqlEnumValueOptions().fromJsonString(jsonString, options);
   }
 
-  static equals(
-    a:
-      | GraphqlEnumValueOptions
-      | PlainMessage<GraphqlEnumValueOptions>
-      | undefined,
-    b:
-      | GraphqlEnumValueOptions
-      | PlainMessage<GraphqlEnumValueOptions>
-      | undefined,
-  ): boolean {
+  static equals(a: GraphqlEnumValueOptions | PlainMessage<GraphqlEnumValueOptions> | undefined, b: GraphqlEnumValueOptions | PlainMessage<GraphqlEnumValueOptions> | undefined): boolean {
     return proto3.util.equals(GraphqlEnumValueOptions, a, b);
   }
 }
@@ -624,41 +471,35 @@ export class GraphqlEnumValueOptions extends Message<GraphqlEnumValueOptions> {
  * @generated from extension: graphql.GraphqlSchemaOptions schema = 2056;
  */
 export const schema = proto3.makeExtension<FileOptions, GraphqlSchemaOptions>(
-  "graphql.schema",
-  FileOptions,
+  "graphql.schema", 
+  FileOptions, 
   () => ({ no: 2056, kind: "message", T: GraphqlSchemaOptions }),
 );
 
 /**
  * @generated from extension: graphql.GraphqlObjectTypeOptions object_type = 2056;
  */
-export const object_type = proto3.makeExtension<
-  MessageOptions,
-  GraphqlObjectTypeOptions
->("graphql.object_type", MessageOptions, () => ({
-  no: 2056,
-  kind: "message",
-  T: GraphqlObjectTypeOptions,
-}));
+export const object_type = proto3.makeExtension<MessageOptions, GraphqlObjectTypeOptions>(
+  "graphql.object_type", 
+  MessageOptions, 
+  () => ({ no: 2056, kind: "message", T: GraphqlObjectTypeOptions }),
+);
 
 /**
  * @generated from extension: graphql.GraphqlInputTypeOptions input_type = 2057;
  */
-export const input_type = proto3.makeExtension<
-  MessageOptions,
-  GraphqlInputTypeOptions
->("graphql.input_type", MessageOptions, () => ({
-  no: 2057,
-  kind: "message",
-  T: GraphqlInputTypeOptions,
-}));
+export const input_type = proto3.makeExtension<MessageOptions, GraphqlInputTypeOptions>(
+  "graphql.input_type", 
+  MessageOptions, 
+  () => ({ no: 2057, kind: "message", T: GraphqlInputTypeOptions }),
+);
 
 /**
  * @generated from extension: graphql.GraphqlFieldOptions field = 2056;
  */
 export const field = proto3.makeExtension<FieldOptions, GraphqlFieldOptions>(
-  "graphql.field",
-  FieldOptions,
+  "graphql.field", 
+  FieldOptions, 
   () => ({ no: 2056, kind: "message", T: GraphqlFieldOptions }),
 );
 
@@ -666,8 +507,8 @@ export const field = proto3.makeExtension<FieldOptions, GraphqlFieldOptions>(
  * @generated from extension: graphql.GraphqlOneofOptions oneof = 2056;
  */
 export const oneof = proto3.makeExtension<OneofOptions, GraphqlOneofOptions>(
-  "graphql.oneof",
-  OneofOptions,
+  "graphql.oneof", 
+  OneofOptions, 
   () => ({ no: 2056, kind: "message", T: GraphqlOneofOptions }),
 );
 
@@ -675,19 +516,17 @@ export const oneof = proto3.makeExtension<OneofOptions, GraphqlOneofOptions>(
  * @generated from extension: graphql.GraphqlEnumOptions enum_type = 2056;
  */
 export const enum_type = proto3.makeExtension<EnumOptions, GraphqlEnumOptions>(
-  "graphql.enum_type",
-  EnumOptions,
+  "graphql.enum_type", 
+  EnumOptions, 
   () => ({ no: 2056, kind: "message", T: GraphqlEnumOptions }),
 );
 
 /**
  * @generated from extension: graphql.GraphqlEnumValueOptions enum_value = 2056;
  */
-export const enum_value = proto3.makeExtension<
-  EnumValueOptions,
-  GraphqlEnumValueOptions
->("graphql.enum_value", EnumValueOptions, () => ({
-  no: 2056,
-  kind: "message",
-  T: GraphqlEnumValueOptions,
-}));
+export const enum_value = proto3.makeExtension<EnumValueOptions, GraphqlEnumValueOptions>(
+  "graphql.enum_value", 
+  EnumValueOptions, 
+  () => ({ no: 2056, kind: "message", T: GraphqlEnumValueOptions }),
+);
+
